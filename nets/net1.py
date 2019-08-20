40ed0cd70b0112cde373a842f573972c72ad49fc
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2019-08-20 21:18:36 +0900

    few changes.

--- nets/net1.py
+++ nets/net1.py
@@ -7,22 +7,25 @@ class Net1(nn.Module):
     def __init__(self):
         super().__init__()
 
-        '''
         shuffle_net = torchvision.models.shufflenet_v2_x1_0(pretrained=True)
         modules = list(shuffle_net.children())[:-1]
+        modules.append(nn.AdaptiveAvgPool2d([1, 1]))
         self.conv_net = nn.Sequential(*modules)
-        '''
+        self.fc = nn.Linear(1024, 1)
 
         '''
         res34 = torchvision.models.resnet34(pretrained=True)
         modules = list(res34.children())[:-1]
         self.conv_net = nn.Sequential(*modules)
+        self.fc = nn.Linear(512, 1)
         '''
 
+        '''
         res50 = torchvision.models.resnet50(pretrained=True)
         modules = list(res50.children())[:-1]
         self.conv_net = nn.Sequential(*modules)
         self.fc = nn.Linear(2048, 1)
+        '''
 
     def forward(self, x):
         batch_size, C, H, W = x.shape
